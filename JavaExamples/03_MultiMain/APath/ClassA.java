854439de6f1fe7c08c9df071cf3db9c7454a46e0
Randall Nagy <randall.nagy@gmail.com>
2022-07-24 03:23:03 -0400

    Muli-Main Selection
    Prep for next Java testing examples ...

---
+++ JavaExamples/03_MultiMain/APath/ClassA.java
@@ -0,0 +1,10 @@
+package APath;
+
+public class ClassA {
+
+    public static void main(String[] args) {
+        System.out.println(new ClassA().getClass().getName());
+    }
+    
+}
+
---
+++ JavaExamples/03_MultiMain/APath/ClassB.java
@@ -0,0 +1,10 @@
+package APath;
+
+public class ClassB {
+
+    public static void main(String[] args) {
+        System.out.println(new ClassB().getClass().getName());
+    }
+    
+}
+
