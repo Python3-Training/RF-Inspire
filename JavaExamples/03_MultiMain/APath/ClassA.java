a354782da61886316f9aa3f62980c1d30935b0ff
Randall Nagy <randall.nagy@gmail.com>
2022-07-28 11:09:37 -0400

    System.exit(0) - Demonstrative
    Eliminating implied exit code.

--- JavaExamples/03_MultiMain/APath/ClassA.java
+++ JavaExamples/03_MultiMain/APath/ClassA.java
@@ -4,6 +4,7 @@ public class ClassA {
 
     public static void main(String[] args) {
         System.out.println(new ClassA().getClass().getName());
+	System.exit(0);
     }
     
 }
--- JavaExamples/03_MultiMain/APath/ClassB.java
+++ JavaExamples/03_MultiMain/APath/ClassB.java
@@ -4,6 +4,7 @@ public class ClassB {
 
     public static void main(String[] args) {
         System.out.println(new ClassB().getClass().getName());
+	System.exit(0);
     }
     
 }
